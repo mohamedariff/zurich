81db441594a53734ff688e3bc64554ffcf99972d
Mohamed Ariff <mohamed-ariff_abdul-rahim@astro.com.my>
2023-08-01 19:50:49 +0800

    center button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,14 @@ function Home() {
           </a>
         </div>
         <h1>Vite + React</h1>
-        <div className="card">
+        <div
+          className="card"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
           <button onClick={() => dispatch(increment((count) => count + 1))}>
             count is {count}
           </button>
